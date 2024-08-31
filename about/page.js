f7ae213dfa19c001842c9c1ee3a1adb3aec1b131
zileuu <86408505+zileuu@users.noreply.github.com>
2024-08-31 15:56:37 +0100

    Add files via upload

---
+++ about/page.js
@@ -0,0 +1,14 @@
+'use client'
+import { useEffect, useState } from "react";
+function Page() {
+    return (
+        <div>
+          <h1>About Us</h1>
+          <p>Welcome to our site. Here&rsquo;s what we do.</p>
+        </div>
+      );
+}
+
+export default Page;
+
+  
