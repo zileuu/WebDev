85ff658cb7c9c44a4ee43a3c252a74daa26509e2
zileuu <86408505+zileuu@users.noreply.github.com>
2024-09-23 17:56:08 +0100

    Update page.js

--- about/page.js
+++ about/page.js
@@ -1,14 +1,27 @@
-'use client'
-import { useEffect, useState } from "react";
-function Page() {
-    return (
-        <div>
-          <h1>About Us</h1>
-          <p>Welcome to our site. Here&rsquo;s what we do.</p>
-        </div>
-      );
-}
+// app/about/page.js
+import  Image  from 'next/image';
 
-export default Page;
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our site. Here&rsquo;s what we do.</p>
+    
+        <h2>Welcome to Next.js Hotel Resort</h2>
+        <p>Find the best hotel rooms and enjoy your stay</p>
+        <p>Home away from home</p>
+        <Image
+  src="img\hotel1.png"
+  alt="Sunset"
+  width={600}
+  height={450}
+  unoptimized={true}
+  loading="eager"
+  layout="fixed"
+/>
+    </div>
+  );
+}
+;
 
   
