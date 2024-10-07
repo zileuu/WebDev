23caa0b42166a0fd15719f6c7dae4034cc7a27ff
zileuu <86408505+zileuu@users.noreply.github.com>
2024-10-07 21:03:07 +0100

    Update layout.js

--- layout.js
+++ layout.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
 import './style.css';
 import {inter} from "next/font/google";
+import { SessionProvider } from "next-auth/react"
 
 
 export default function Layout({ children }) {
@@ -25,34 +26,80 @@ export default function Layout({ children }) {
     setUser(null);
   };
 
+
   return (
-    <html>
-      <body>
-        <div>
+
+  
+<body>
+   <div>
+
+      <header>
+        <nav>
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/search">Search</Link>
+          {user ? (
+            <>
+              <span>Welcome, {user.username}</span>
+              <button onClick={handleLogout}>Logout</button>
+            </>
+          ) : (
+            <>
+              <Link href="/login">Login</Link>
+              <Link href="/signup">SignUp</Link>
+              <Link href="/booking">Booking</Link>
+
+            </>
+          )}
+        </nav>
+      </header>
     
-          <header>
-            <nav>
-              <Link href="/">Home</Link>
-              <Link href="/about">About</Link>
-              <Link href="/search">Search</Link>
-              {user ? (
-                <>
-                  <span>Welcome, {user.username}</span>
-                  <button onClick={handleLogout}>Logout</button>
-                </>
-              ) : (
-                <>
-                  <Link href="/login">Login</Link>
-                  <Link href="/signup">Signup</Link>
-                </>
-              )}
-            </nav>
-          </header>
-          <main>{children}</main>
+        <table>
+      <tr >
+        <td >Floors 0 to 12(OFFER 1)</td>
+        <td  >  Hostel share experience</td>
+        <td class="dead-link soldout">
+          <h4><span class="label label-danger">Sold Out! </span></h4>
+        </td>
+      </tr>
+      <tr>
+        <td>Floors 13 to 26(OFFER 2) </td>
+        <td>  Hotel private experience</td>
+        <td><a href="/booking">Book now</a></td>
+      </tr>
+      <tr>
+      <td>Floors 27 to 35(OFFER 3)</td>
+        <td>  Hotel Luxury experience</td>
+        <td><a href="/booking"> Book now</a></td>
+      </tr>
+      <tr>
+      <td>Outside Area(OFFER 4)</td>
+        <td>  Our campsite offer</td>
+        <td><a href="/booking"> Book now</a></td>
+      </tr>
+
+      </table>
+
+
+    
+    </div><main>
+        <SessionProvider>
+          {children}
+        </SessionProvider></main>
       
-        </div>
+
+        </body>
+
+
+
+
       
-      </body>
+
+
+
+  );
+}
+
      
     </html>
   );
